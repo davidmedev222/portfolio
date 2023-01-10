e833500432826c3b3504da10a85865961ee65269
davidmedev222 <davidmedev@gmail.com>
2023-01-10 20:47:14 -0300

    chore: add about interface

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,39 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/global/GlobalContext';
+import { AboutSkill } from './AboutSkill';
+
 const About = () => {
+  const { about } = useContext(GlobalContext);
+
+  const { span1, title1, span2, title2, description1, description2, description3 } = about;
+
+  const aboutSkill = about.skills.map((e) => <AboutSkill key={e} skill={e} />);
+
   return (
-    <>
-      <h1>ABOUT</h1>
-    </>
+    <section id="aboutme" className="about">
+      {/* TITLE */}
+      <h3 className="about-title">
+        <span className="about-span">{span1}</span>
+        {title1}
+        <span className="about-span"> {span2}</span>
+        {title2}
+      </h3>
+      {/* DESCRIPTION */}
+      <article className="about-art">
+        <h4 className="about-subtitle">DESCRIPTION</h4>
+        <p className="about-description">{description1}</p>
+        <p className="about-description">{description2}</p>
+        <p className="about-description">{description3}</p>
+      </article>
+      {/* SKILLS */}
+      <article className="about-art">
+        <h4 className="about-subtitle">SKILLS</h4>
+        <ul className="about-ul">
+          {/* COMPONENT */}
+          {aboutSkill}
+        </ul>
+      </article>
+    </section>
   );
 };
 
---
+++ src/pages/about/AboutSkill.jsx
@@ -0,0 +1,5 @@
+const AboutSkill = ({ skill }) => {
+  return <li className="about-li">{skill}</li>;
+};
+
+export { AboutSkill };
