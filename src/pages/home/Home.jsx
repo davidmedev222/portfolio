57fe9cd3eb2e905eb36bf5af8e4871222e657d45
davidmedev222 <davidmedev@gmail.com>
2023-01-10 20:57:11 -0300

    feat: add imports from components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,24 @@
 import { Hero } from '../../components/hero/Hero';
+import { ParallaxLines } from '../../components/parallax/lines/ParallaxLines';
+import { Projects, About, Contact } from '../export';
 
 const Home = () => {
   return (
-    <main>
+    <main className="main">
+      {/* COMPONENT */}
       <Hero />
+      {/* COMPONENT */}
+      <ParallaxLines />
+      {/* COMPONENT */}
+      <Projects />
+      {/* COMPONENT */}
+      <ParallaxLines />
+      {/* COMPONENT */}
+      <About />
+      {/* COMPONENT */}
+      <ParallaxLines />
+      {/* COMPONENT */}
+      <Contact />
     </main>
   );
 };
