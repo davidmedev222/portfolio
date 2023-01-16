62cbaae551214b731cace64e9fcd7a54806ecd1c
davidmedev222 <davidmedev@gmail.com>
2023-01-16 20:07:04 -0300

    chore(styles): add new styles to contact component

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,12 +5,14 @@ import { GlobalContext } from '../../context/global/GlobalContext';
 const Contact = () => {
   const { contact } = useContext(GlobalContext);
 
+  const { span, title } = contact;
+
   return (
     <section id="contact" className="contact">
       {/* TITLE */}
       <h3 className="contact-title">
-        <span className="contact-span">{contact.span}</span>
-        {contact.title}
+        <span className="contact-span">{span}</span>
+        {title}
       </h3>
       {/* COMPONENT */}
       <IconsSocial />
